25e3de3a5aa8ff37ea2eb9b8b9d1fe5f633abfa2
ShiromiyaG <86894155+ShiromiyaG@users.noreply.github.com>
2022-07-27 13:56:51 -0300

    Adicionar arquivos de projeto.

---
+++ Tamagotchi/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace Tamagotchi
+{
+    internal class Program
+    {
+        //main
+        static void Main(string[] args)
+        {
+            //instancias
+            Random random = new Random();
+            Console.WriteLine("Digite um nome para seu tamagotchi: ");
+            String nome = Console.ReadLine();
+            Tamagotchi tama = new Tamagotchi(nome);
+
+            //atributos
+            bool morto = false;
+            while (!morto)
+            {
+                int escolha = random.Next(1, 3);
+                Console.WriteLine(tama);
+                switch (escolha)
+                {
+                    case 1:
+                        Program.sentirSono(tama);
+                        break;
+                    case 2:
+                        Program.sentirFome(tama);
+                        break;
+                    case 3:
+                        Program.ficarEntediado(tama);
+                        break;
+                }
+                if (tama.idade >= 15)
+                {
+                    Console.WriteLine("\n" + nome + " morreu de velhice");
+                    System.Threading.Thread.Sleep(10000);
+                    morto = true;
+                }
+                else if (tama.quilos >= 20)
+                {
+                    Console.WriteLine("\n" + nome + " explodiu de tanto comer");
+                    System.Threading.Thread.Sleep(10000);
+                    morto = true;
+                }
+                else if (tama.quilos <= 0)
+                {
+                    Console.WriteLine("\n" + nome + " ficou desnutrido e morreu");
+                    System.Threading.Thread.Sleep(10000);
+                    morto = true;
+                }
+            }
+            
+
+        }
+
+        //metodos
+        static void sentirSono(Tamagotchi tama)
+        {
+            Console.WriteLine("\nEscolha uma das opções: ");
+            Console.WriteLine("\n1) Dormir \n2) Permanecer acordado");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            if(tama.sono == 5)
+            {
+                escolha = 3;
+                tama.sono = 0;
+            }
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("\n" + "O " + tama.nome + " domiu");
+                    tama.idade += 1;
+                    break;
+                case 2:
+                    Console.WriteLine("\n" + "O " + tama.nome + " permaneceu acordade");
+                    tama.sono += 1;
+                    break;
+                case 3:
+                    Console.WriteLine("\n" + "O " + tama.nome + " chegou ao limite e dormiu");
+                    tama.idade += 1;
+                    break;
+            }
+        }
+        static void sentirFome(Tamagotchi tama)
+        {
+            Console.WriteLine("\nEscolha uma das opções: ");
+            Console.WriteLine("\n1) Comer muito \n2) Comer pouco \n3) Não comer");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("\n" + "O " + tama.nome + " comeu muito");
+                    tama.quilos += 5;
+                    break;
+                case 2:
+                    Console.WriteLine("\n" + "O " + tama.nome + " comeu pouco");
+                    tama.quilos += 1;
+                    break;
+                case 3:
+                    Console.WriteLine("\n" + "O " + tama.nome + " não comeu e emagreceu");
+                    tama.quilos -= 2;
+                    break;
+            }
+        }
+        static void ficarEntediado(Tamagotchi tama)
+        {
+            Console.WriteLine("\nEscolha uma das opções: ");
+            Console.WriteLine("\n1) Correr 10 minutos \n2) Caminhar 10 minutos");
+            int escolha = Convert.ToInt32(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("\n" + "O " + tama.nome + " correu e comeu muito");
+                    tama.quilos += 1;
+                    break;
+                case 2:
+                    Console.WriteLine("\n" + "O " + tama.nome + " caminhou e comeu um pouco");
+                    break;
+            }
+        }
+    }
+}
---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagotchi
+{
+    internal class Tamagotchi
+    {
+        //atributos
+        public string nome { get; set; }
+        public int sono { get; set; }
+        public float quilos { get; set; }
+        public int entediado { get; set; }
+        public int idade { get; set; }
+
+        public Tamagotchi(string nome)
+        {
+            this.nome = nome;
+            int sono = 0;
+            int quilos = 1;
+            int entediado = 0;
+            int idade = 0;
+        }
+        public override string ToString()
+        {
+            return "\n" + nome + " esta com:\n" + idade + " dias de idade\nPesando " + quilos + "Kg\n";
+        }
+    }
+}
